ee1b248c44ca7e8b6d4ffb033fe29c3b91732921
parix1999 <parisottoedoardo@gmail.com>
2021-09-07 15:49:26 +0200

    basi, nuova classe

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?php
 
+class Eshop {
+    // Nome dello shop:
+    public $name;
+    // Indirizzo, entrabi pubblici perchè tutti possono vederli:
+    public $adress;
+
+    // Li assegno gli argomenti che voglio io:
+    public function __construct(string $nomeShop, string $indirizzo) {
+        $this->name = $nomeShop;
+        $this->adress = $indirizzo;
+    }
+
+}
+
+// Assegno ad una variabile il mio oggetto con dentro nelle parentesi gli argomenti che li voglio passare:
+$shop = new Eshop('TuttoPerTutti', 'Via della Grazia');
+var_dump($shop);
